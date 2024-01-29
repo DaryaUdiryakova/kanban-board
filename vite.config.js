2648068271263618aff222a38437290d1c18a911
darya.khodoreva <darya.khodoreva@rcntec.com>
2024-01-30 01:06:45 +0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    reactivityTransform: true
+  })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/kanban-board/'
-    : '/'
+  base: '/kanban-board/',
+
 })
